1a451992d0791182753988e0d7c409de8d9915e2
agajo <madaopc@gmail.com>
2021-04-06 17:00:40 +0900

    added background of Simulated

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.ltr,
-      child: Padding(
-        padding: const EdgeInsets.all(4),
-        child: Simulated.sample(
-          child: MaterialApp(
-              theme: myTheme,
-              onGenerateTitle: null, // TODO: L10n.of(context).appTitle
-              home: const MainPage()),
-        ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints.expand(),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.orange[100]!, Colors.pink[100]!],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4),
+            child: Simulated.sample(
+              child: MaterialApp(
+                  theme: myTheme,
+                  onGenerateTitle: null, // TODO: L10n.of(context).appTitle
+                  home: const MainPage()),
+            ),
+          ),
+        ],
       ),
     );
   }
