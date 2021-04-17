3276892a4a53b6f53d9f08e0698111c8ab663bed
agajo <madaopc@gmail.com>
2021-04-18 00:05:37 +0900

    modified DrawerHeader. not show the device frame if mobile

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -12,8 +12,21 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(
-            child: null,
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Theme.of(context).colorScheme.background,
+              Theme.of(context).colorScheme.primary,
+            ])),
+            currentAccountPicture: ClipOval(
+              child: Image.asset('assets/profile_image.png'),
+            ), // TODO: アカウント画像前用意した奴assetsに追加して貼る
+            accountEmail: const Text('masanada@gmail.com'),
+            accountName: const Text('@agajo_tech'),
+            onDetailsPressed: () {
+              print("clicked!!"); // TODO: ツイッターにリンク飛べるように
+            },
+            arrowColor: Colors.transparent,
           ),
           AboutListTile(),
           SwitchListTile(
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,16 @@ class MyApp extends StatelessWidget {
   const MyApp();
   @override
   Widget build(BuildContext context) {
+    final size = WidgetsBinding.instance!.window.physicalSize;
+    final aspectRatio = size.width / size.height;
+    print(aspectRatio);
     return Directionality(
       textDirection: TextDirection.ltr,
       child: Stack(
         alignment: Alignment.center,
         children: [
           ConstrainedBox(
-            constraints: BoxConstraints.expand(),
+            constraints: const BoxConstraints.expand(),
             child: DecoratedBox(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
@@ -29,12 +32,14 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(4),
-            child: Simulated.sample(
-              child: const MainPage(),
-            ),
-          ),
+          aspectRatio > 2.0 / 3.0
+              ? const Padding(
+                  padding: EdgeInsets.all(4),
+                  child: Simulated.sample(
+                    child: MainPage(),
+                  ),
+                )
+              : const MainPage(),
         ],
       ),
     );
